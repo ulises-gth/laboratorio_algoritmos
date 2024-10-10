acbb546c236704a62023eec3dc28e3df62173c0e
ulises-gth <ulises.j.arguello@gmail.com>
2024-10-09 22:04:27 -0300

    Add files via upload

--- proyectodia5.py
+++ proyectodia5.py
@@ -8,6 +8,7 @@ numero_ganador = randint(1,100)
 
 while contador < numero_intentos:
     numerodel_usuario=int(input("Ingresa tu numero: "))
+    contador=contador+1
     if numerodel_usuario < 1 or numerodel_usuario > 100:
         print("El numero que debe ingresar debe estar entre 1 y 100: ")
     if numerodel_usuario != numero_ganador:
@@ -16,11 +17,11 @@ while contador < numero_intentos:
         print("El numero que penso es mayor a ese numero")
     if numerodel_usuario > numero_ganador:
         print("El numero que penso es menor a ese numero")
-    contador=contador+1
     if numerodel_usuario == numero_ganador:
+        print("Te tomo la cantidad",contador,"de intentos")
         print("Felicidades su numero es CORRECTO")    
         break
 else:
-    print("Perdiste, el numero que habia pensado", numero_ganador)
+    print("Perdiste, el numero que habia pensado era", numero_ganador)
 
 print("Este trabajo fue realizado por Ulises Arguello")
